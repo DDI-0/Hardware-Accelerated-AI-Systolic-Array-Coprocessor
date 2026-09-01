751125a9054bb141dfc3ba2ffe8abe9e8e11e8bf
PeterD. <136271811+DDI-0@users.noreply.github.com>
2026-09-01 18:46:19 -0400

    Delete test_read_ram.tcl

--- test_read_ram.tcl
+++
@@ -1,8 +0,0 @@
-set jtag_master [lindex [get_service_paths master] 0]
-open_service master $jtag_master
-
-puts "Reading RAM at 0x0000..."
-set ram_data [master_read_32 $jtag_master 0x00000000 8]
-puts $ram_data
-
-close_service master $jtag_master
